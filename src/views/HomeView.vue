902175eb5908e9a63a94fd78695025bfe3d17530
h3nrey <pedro.novaes.education@gmail.com>
2022-08-11 19:17:39 -0300

    decoupe out location section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -187,42 +187,7 @@
           />
         </div>
       </section>
-      <section
-        id="ourlocations"
-        class="bg-red text-white flex flex-col items-center gap-6 text-center py-10 px-6"
-      >
-        <div name="section__info" class="flex flex-col items-center">
-          <h2 class="font-bold font-title text-yellow text-[1rem] uppercase">
-            our location
-          </h2>
-          <p class="uppercase font-title font-bold text-[2.5rem]">
-            FIND THE PIZZERIA NEAR YOU
-          </p>
-          <p class="font-text text-sm">
-            Cursus ultricies in maecenas pulvinar ultrices integer quam amet,
-            semper dictumst sit interdum ut venenatis pellentesque.
-          </p>
-        </div>
-
-        <div name="locations__grid" class="grid grid-rows-4 gap-6">
-          <LocationContainer
-            locationTitle="Bologna"
-            locationDesc="Via delle Lame, 55, 40122 Bologna BO, Italy. +39 123 456 7890"
-          />
-          <LocationContainer
-            locationTitle="Rome"
-            locationDesc="Via di S. Vincenzo, 12/34A, 00123 Roma RM, Italy. +39 123 456 7890"
-          />
-          <LocationContainer
-            locationTitle="Venice"
-            locationDesc="S. Marco, 615, 30124 Venezia VE, Italy. +39 123 456 7890"
-          />
-          <LocationContainer
-            locationTitle="Florence"
-            locationDesc="Viale Belfiore, 50, 50144 Firenze FI, Italy. +39 123 456 7890"
-          />
-        </div>
-      </section>
+      <OurLocations />
     </main>
     <FooterComponent />
   </div>
@@ -249,6 +214,7 @@ import HorizontalDeal from "@/components/HorizontalDeal.vue";
 import LocationContainer from "@/components/LocationContainer.vue";
 import Logo from "@/components/Logo.vue";
 import FooterComponent from "../components/footerComponent.vue";
+import OurLocations from "@/components/sections/OurLocations.vue";
 
 export default {
   name: "HomeView",
@@ -265,7 +231,8 @@ export default {
     LocationContainer,
     Logo,
     FooterComponent,
-  },
+    OurLocations
+},
   data() {
     return {
       differentials: [
