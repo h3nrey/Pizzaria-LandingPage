6ff36c92517bd9bd167afe3719bbbe8d009fcbda
h3nrey <pedro.novaes.education@gmail.com>
2022-09-02 10:59:58 -0300

    adding transition to input outline

--- src/components/sections/ContactForm.vue
+++ src/components/sections/ContactForm.vue
@@ -10,12 +10,12 @@
         <legend class="capitalize text-darkGray font-medium">name</legend>
         <div class=" grid grid-cols-2 gap-4">
           <div class="flex flex-col">
-          <input name="from_name" type="text" id="firstname" class=" bg-gray visited:outline-darkGray rounded-sm p-[0.7rem]" placeholder="first">
-          <label for="firstname">first</label>
+          <input name="from_name" type="text" id="firstname" class="field " placeholder="first">
+          <label for="firstname" class=" text-black">first</label>
           </div>
           <div class="flex flex-col">
-            <input name="from_lastname" type="text" id="lastname" class=" bg-gray visited:outline-darkGray rounded-sm p-[0.7rem]" placeholder="first">
-            <label for="lastname">last</label>
+            <input name="from_lastname" type="text" id="lastname" class="field" placeholder="first">
+            <label for="lastname" class=" text-black">last</label>
           </div>
         </div>
         
@@ -25,36 +25,36 @@
       <div class="flex flex-col gap-6">
         <div class="flex flex-col text-darkGray font-medium">
           <label for="email" class=" capitalize">email</label>
-          <input name="reply_to" type="email" id="email" class=" bg-gray visited:outline-darkGray rounded-sm p-[0.7rem]" placeholder="Email Address">
+          <input name="reply_to" type="email" id="email" class="field" placeholder="Email Address">
         </div>
 
         <div class="flex flex-col text-darkGray font-medium">
           <label for="phone" class=" capitalize">phone</label>
-          <input name="phone" type="text" id="phone" class=" bg-gray visited:outline-darkGray rounded-sm p-[0.7rem]" placeholder="Phone Number">
+          <input name="phone" type="text" id="phone" class="field" placeholder="Phone Number">
         </div>
       </div>
 
       <div class="flex flex-col gap-6">
         <div class="flex flex-col text-darkGray font-medium">
           <label for="subject" class="capitalize">subject</label>
-          <input name="subject" type="subject" id="subject" class=" bg-gray visited:outline-darkGray rounded-sm p-[0.7rem]" placeholder="Subject">
+          <input name="subject" type="subject" id="subject" class="field" placeholder="Subject">
         </div>
         
         <div class="flex flex-col">
           <label for="location" class="capitalize text-darkGray font-medium">Select Location</label>
           <div class="relative z-0 w-full cursor-pointer">
-            <span class="absolute z-10 top-1/2 left-[100%] -translate-y-1/2 -translate-x-[100%] p-4 height-full w-[10%] outline-1 outline-darkGray pointer-events-none">
-              <CaretIcon class=" text-black" :size="20"/>
+            <span class="absolute z-10 top-1/2 left-[100%] -translate-y-1/2 -translate-x-[100%] p-4 height-full w-[12%] outline-1 outline-darkGray pointer-events-none">
+              <CaretIcon class=" text-darkGray" :size="20"/>
             </span>
             <select 
-            name="restaurant"
-          v-model="selected" 
-          id="location" 
-          class="bg-gray text-darkGray visited:outline-darkGray rounded-sm p-[0.7rem] appearance-none w-full cursor-pointer" 
-          >
-            <option disabled value="default" class="">Select a Restaurant</option>
-            <option v-for="(option, index) in options" class=" capitalize" :value="option.value" :key="index">{{option.text}}</option>
-            </select>
+              name="restaurant"
+              v-model="selected" 
+              id="location" 
+              class="field appearance-none w-full cursor-pointer" 
+            >
+              <option disabled value="default" class=" text-darkGray font-semibold">Select a Restaurant</option>
+              <option v-for="(option, index) in options" class=" capitalize text-darkGray font-semibold" :value="option.value" :key="index">{{option.text}}</option>
+              </select>
           </div>
           
 
@@ -62,7 +62,7 @@
 
         <div class="flex flex-col text-darkGray font-medium">
           <label for="comment" class="capitalize">comment or message</label>
-          <textarea name="feedback" id="comment" cols="30" rows="5" class="bg-gray visited:outline-darkGray rounded-sm p-[0.7rem]"></textarea>
+          <textarea name="feedback" id="comment" cols="30" rows="5" class="field"></textarea>
         </div>
         
         <button @click="sendEmail" type="submit" class=" hover:bg-red text-[1rem] w-fit py-[0.875rem] px-[1.3125rem] uppercase font-mont font-bold rounded-[5px] bg-opacity-0 outline outline-2 hover:bg-opacity-100 active:bg-opacity-100 transition-all duration-300 text-red outline-red hover:text-white font-semibold tracking-widest leading-[initial]">submit</button>
@@ -73,7 +73,18 @@
 </template>
 
 <style scoped>
-  select::after{
+  .field{
+    outline: 2px solid transparent;
+    transition: all 400ms;
+    color: #4a4a4a;
+    font-weight: 600;
+    background-color: #EBEBEB;
+    border-radius: 0.25rem;
+    padding: 0.7rem;
+  }
+
+  .field:visited, .field:focus, .field:focus-visible, .field:hover{
+    outline-color: #4a4a4a;
 
   }
 </style>
