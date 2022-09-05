037db1611c27ce00af432de1688dd971923de9d9
h3nrey <pedro.novaes.education@gmail.com>
2022-09-05 18:53:25 -0300

    adding more info section to about us page

---
+++ src/components/sections/AboutMoreinfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="py-10 px-6 md:px-16" :class="sectionBg">
+    <span>
+      <h2 class=" uppercase font-title font-[500] tracking-[2px]" :class="sectionColor">{{sectionHeader}}</h2>
+    </span>
+    <div class="flex flex-col gap-8">
+      <div class="flex flex-col gap-4">
+        <h3 class="text-white font-[500] uppercase text-[1.75rem] font-title md:text-[2.5rem]">{{sectionBigText}}</h3>
+        <p class=" text-gray">Semper dictumst sit interdum venenatis pellentesque nunc libero vestibulum velit sit aliquet pellentesque vitae.</p>
+      </div>
+      <div v-for="(info, index) in infos" :key="infos" class="flex flex-col md:flex-row gap-4">
+        <slot name="item1" v-if="index == 0"></slot>
+        <slot name="item2" v-if="index == 1"></slot>
+        <div class="flex flex-col gap-1">
+          <h4 class=" text-[500] font-title uppercase text-white md:text-[1.375rem]">{{info.header}}</h4>
+          <p class="text-gray">{{info.paragraph}}</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import PhoneIcon from "@/components/icons/PhoneIcon.vue";
+
+const props = defineProps({
+  sectionBg: String,
+  sectionColor: String,
+  sectionHeader: String,
+  sectionBigText: String,
+  infos: Array,
+})
+
+console.log(props);
+
+</script>
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,58 +1,66 @@
 <template>
   <div
-    class="home w-[100vw] before:absolute before:bg-history before:top-0 before:left-0 before:h-[47vh] before:w-[100vw] before:bg-center before:bg-cover before:z-[-1] before:brightness-50 md:before:h-[75vh] py-20 md:py-0 md:mt-20"
+    class="home w-[100vw] before:absolute before:bg-history before:top-0 before:left-0 before:h-[47vh] before:w-[100vw] before:bg-center before:bg-cover before:z-[-1] before:brightness-50 md:before:h-[75vh] pt-20 md:py-0 md:mt-20"
   >
     <div class="flex flex-col align-center gap-6 px-6 mt-0">
       <ViewTitle title="about" />
       <p class=" text-white font-text text-center md:px-[30%]">Dignissim sed suscipit mattis neque, in nibh blandit at nec in urna tristique ornare aliquam orci augue vestibulum dignissim vel aliquam.</p>
     </div>
-    <main class="py-[10rem] px-6">
-      <div id="history__container" class="md:flex w-full rounded-2xl md:px-10 flex flex-col gap-6">
-        <span>
-          <h2 class=" uppercase text-red font-title font-[500] tracking-[2px]">our story</h2>
-        </span>
-        
-        <div class="flex flex-col gap-10 md:flex-row">
-          <div class="flex flex-col gap-10">
-            <div class="flex flex-col">
-              <h1 class=" text-black uppercase font-title font-[500] text-[2.5rem] md:text-[4rem]">authetic italian pizzeria</h1>
-              <p class="font-semibold text-black">Varius eu mauris est vitae ultrices et, justo, at in ut faucibus libero pellentesque facilisis molestie suspendisse lacinia gravida sed habitasse diam nec nulla.</p>
-            </div>
-            <div class="flex flex-col gap-10 md:flex-row-reverse">
-              <div class="flex flex-col gap-6">
-                <section v-for="(section, index) in sections" :key="index" class="flex flex-col gap-2">
-                  <h2 class="font-title text-black uppercase font-bold md:text-[1.375rem]">{{section.header}}</h2>
-                  <p class="text-darkGray font-text">{{section.paragraph}}</p>
-                </section>
-              </div>
-            
-            <div name="Founder__container" class="flex flex-col gap-4">
-              <div class="bg-founder bg-cover bg-right sepia contrast-75 saturate-50 w-[10.69rem] h-[10.69rem] md:w-[21.9rem] md:h-[21.9rem]"></div>
-              <div class="flex flex-col gap-2">
-                <h2 class="font-bold font-title uppercase md:text-[1.6rem]">kiriyama akari</h2>
-                <p class="font-semibold text-darkGray font-text">Founder of the sushi man</p>
-              </div>
-            </div>
-            </div>
-          </div>
-          <div name="first local" class="bg-firstLocal bg-cover bg-center sepia contrast-75 saturate-50 w-full h-[32rem] md:h-[37.5rem]"></div>
-        </div>
-        
+    <main class="pt-[10rem]">
+      <HistorySection />
+      <div class="flex flex-col md:flex-row">
+        <AboutMoreinfo sectionBg="bg-red" sectionColor="text-yellow" sectionHeader="sushi sun" sectionBigText="INCREDIBLE DISHES START HERE" :infos="infos.info1">
+          <template #item1>
+            <ph-storefront :size="64" weight="fill" class="text-yellow" />
+          </template>
+
+          <template #item2>
+            <ph-fish :size="64" weight="fill" class="text-yellow" />
+          </template>
+        </AboutMoreinfo>
+
+        <AboutMoreinfo sectionBg="bg-yellow" sectionColor="text-red" sectionHeader="authentic" sectionBigText="RECIPE WITH ROOTS" :infos="infos.info2">
+          <template #item1>
+            <ph-fish :size="64" weight="fill" class="text-red" />
+          </template>
+
+          <template #item2>
+            <ph-scroll :size="64" weight="fill" class="text-red" />
+          </template>
+        </AboutMoreinfo>
       </div>
+      
     </main>
   </div>
 </template>
 <script setup>
 import ViewTitle from "@/components/ViewTitle.vue";
+import HistorySection from "../components/sections/HistorySection.vue";
+import AboutMoreinfo from "../components/sections/AboutMoreinfo.vue";
+import PhoneIcon from "@/components/icons/PhoneIcon.vue";
+import TwitterIcon from "@/components/icons/TwitterIcon.vue";
+import { PhStorefront, PhFish, PhScroll } from "phosphor-vue";
 
-const sections = [
-  {
-    header: "it's all about a family",
-    paragraph: "Imperdiet orci, volutpat consequat fermentum, proin tempus et tellus, vulputate adipiscing auctor nulla in malesuada et amet, aliquam laoreet mauris gravida consectetur malesuada est ultricies diam vestibulum velit sit aliquet pellentesque vitae."
-  },
-  {
-    header: "generations in the making",
-    paragraph: "Imperdiet orci, volutpat consequat fermentum, proin tempus et tellus, vulputate adipiscing auctor nulla in malesuada et amet, aliquam laoreet mauris gravida consectetur malesuada est ultricies diam vestibulum velit sit aliquet pellentesque vitae."
-  }
-]
+const infos = {
+  info1: [
+    {
+      header: "4,200 SQUARE FEET OF SPACE",
+      paragraph: "Bibendum rhoncus amet tellus ornare lobortis sit aliquet elit, hac nisi, mauris ac tellus velit sollicitudin."
+    },
+    {
+      header: "HERITAGE YOU CAN TASTE",
+      paragraph: "Pretium, condimentum magna amet egestas ligula pretium magna rutrum a in arcu, mauris porttitor fermentum."
+    }
+  ],
+  info2: [
+    {
+      header: "AUTHENTIC japanese sushi",
+      paragraph: "Bibendum rhoncus amet tellus ornare lobortis sit aliquet elit, hac nisi, mauris ac tellus velit sollicitudin."
+    },
+    {
+      header: 'HANDMADE "SECRET" INGREDIENTS',
+      paragraph: "Pretium, condimentum magna amet egestas ligula pretium magna rutrum a in arcu, mauris porttitor fermentum."
+    }
+  ],
+}
 </script>
